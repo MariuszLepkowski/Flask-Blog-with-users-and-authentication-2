a00f55fd4ac3dae78b560da5fa78d79756d9e7bd
Mariusz <mariusz.lepkowski23@gmail.com>
2023-10-16 15:59:45 +0200

    Delete email characters limit

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ class Comment(db.Model):
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String, unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
     posts = db.relationship('BlogPost', back_populates='author')
