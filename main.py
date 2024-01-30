fde55e4d57c639e878c5cd793c576e0c5ccc2f93
Mariusz <mariusz.lepkowski23@gmail.com>
2024-01-30 14:42:28 +0100

    Update project configuration inside main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 ckeditor = CKEditor(app)
-Bootstrap5(app)
+bootstrap = Bootstrap5(app)
 
 # TODO: Configure Flask-Login
 login_manager = LoginManager()
@@ -65,7 +65,7 @@ class Comment(db.Model):
     parent_post_id = db.Column(db.Integer, db.ForeignKey('blog_posts.id'), nullable=False)
 
 
-# TODO: Create a User table for all your registered users. 
+# TODO: Create a User table for all your registered users.
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
@@ -114,7 +114,7 @@ def register():
     return render_template("register.html", form=form)
 
 
-# TODO: Retrieve a user from the database based on their email. 
+# TODO: Retrieve a user from the database based on their email.
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -237,4 +237,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5002)
+    app.run(debug=True, port=5002)
